c7e0dd8fe7fa3abc1d79cd307b30060bcdec9ff4
Anne Lindenberg <lindenba@onid.orst.edu>
2015-04-25 16:43:06 -0700

    Api in array is still working and output to screen.  Worked on/got it to work the favorites button and getting the favorites to move to favorites list. When reload page the favorites still stay there.  If I add more search items and click on favorite it moves over.  If I reload the page old favorites disappear, but new favorite stays there. Need to take a break

--- assignment3.js
+++ assignment3.js
@@ -1,164 +1,151 @@
-var req;
-//only return 1 to 5 page numbers
-function getGists() {
-  //url to get gists set to variable url
-  var url = 'https://api.github.com/gists';
-  var pageNums = document.getElementById('pageNum');
-  //only get 1 to 5 pages from gist
-  if (pageNums < 1 || pageNums > 5)
-  {
-    pageNums = 1;
-    console.log('Invalid amount of pages, return 1 page');
-  }
-  console.log('value in page number field: ' + pageNums);
-  for (var i = 1; i <= pageNums; ++i) {
-    if (i == 1)
-    {
-      reqGists(url, i);
-    }
-    else if (i == 2)
-    {
-      url += '?page=2';
-      reqGists(url, i);
-    }
-    else
-    {
-      url = url.slice(0, url.length - 1);
-      url += 1;
-      reqGists(url, i);
-    }
-  }
-}
+var gistsArray = [];
 //make an AJAX request and store results
 function reqGists(url, page)
 {
-  var temp = 'results' + page;
+  var pageSelect = document.getElementsByName('page');
+  var url = 'https://api.github.com/gists';
+  if (pageSelect[0].value > 5)
+  {
+    pageSelect[0].value = 5;
+  }
+  for (var i = 1; i <= pageSelect[0].value; i++) {
   //make a request
-  if (window.XMLHttpRequest) {
+  if (window.XMLHttpRequest)
+  {
     var req = new XMLHttpRequest();
-  } else if (window.ActiveXObject) {
+  } else if (window.ActiveXObject)
+  {
     req = new ActiveXObject('Microsoft.XMLHTTP');
   }
   if (!req)
   {
     throw 'Unable to create HttpRequest.';
   }
+  var params = {
+      page: pageSelect
+    };
+    url += '?' + urlStringify(params, i);
   req.onreadystatechange = function()
   {
     if (req.readyState === 4)
     {
-      if (req.status === 200)
-      {
-        var gists = this.responseText;
-        var json = JSON.parse(gists);
-        loopGists(page, json);
-        outputGists(page);
-      }
+      if (req.status === 200) {
+        var gists = JSON.parse(this.responseText);
+        var gistsFinal = [];
+        var sizeFinal = 0;
+          for (var i = 0; i < gists.length; i++)
+          {
+            for (var key in gists[i].files)
+            {
+                  gistsFinal[sizeFinal] = gists[i];
+                  sizeFinal++;
+            }
+          }
+        }
+        gistsArray = gistsFinal;
+      //function to create list get from array and put in search results column
+        createResultList(gistsArray, document.getElementById('search-results'));
     }
   };
-  req.open('GET', 'https://api.github.com/gists', true);
+  req.open('GET', url);
   req.send();
   }
-  //pushes gist to the session storage
-function loopGists(page, json)
+}
+
+window.onload = function()
+{
+  console.log('Begin!');
+  sessionStorage.clear();
+  createDisplaylist();
+}
+
+function urlStringify(param, i) {
+  var string = [];
+  for (var prop in param) {
+   var s = encodeURIComponent(prop) + '=' + encodeURIComponent(i);
+    string.push(s);
+  }
+  return string.join('&');
+}
+//create the result list from the gist api
+function createResultList(array, list)
+{
+  var list = document.getElementById('gists');
+  var ul = document.createElement('ul');
+  for (var i = 0; i < array.length; i++)
   {
-    var fileholder, descrip, files, filename;
-    var saveGist = true;
-    var allUnchecked = true;
-    if ((document.getElementById('python').checked) ||
-    (document.getElementById('json').checked) ||
-    (document.getElementById('javascript').checked) ||
-    (document.getElementById('sql').checked))
-    {
-      allUnchecked = false;
-    }
-    //any boxes are checked
-    if (!allUnchecked)
-    {
-      for (var x in sessionStorage)
-      {
-        var checkText = sessionStorage.getItem(x);
-        if (checkText == null)
-        {
-          break;
-        }
-      }
-    }
-    //start loop through json results
-    for (var i in json)
-    {
-      var language = [];
-      descrip = json[i].descrip;
-      href = json[i].html_url;
-      //get files object
-      files = json[i].files;
-      //get the filename;
-      for (var f in files)
-      {
-        fileholder = files[f];
-        filename = fileholder.filename;
-      }
-      var storeItem = {
-        'filename': filename,
-        'description': descrip,
-        'href': href,
-        'language': language,
-        'page': page
-      };
-      var keyname = json[i].id;
-      //if keyname has a match already in favorites don't add
-      if (localStorage.getItem(keyname))
-      {
-        saveGist = false;
-      }
-      else
-      {
-        if (!allUnchecked)
-        {
-          saveGist = false;
-        }
-      }
-      if (saveGist)
-      {
-        sessionStorage.setItem(keyname, JSON.stringify(storeItem));
-      }
-      language = null;
-      saveGist = true;
-    }//end loop through json results
-    console.log('how about here');
+  var theFirstChild = list.firstChild;
+  var conDiv = document.createElement('li');
+  list.insertBefore(conDiv, theFirstChild);
+  conDiv.id = array[i].id;
+  if (array[i].description == null || array[i].description.length == 0)
+  {
+    var item = document.createElement('div');
+    item.innerHTML += '<a href ="' + array[i].html_url +
+     '">' + 'No Description' + '</a>';
+     conDiv.appendChild(item);
   }
-function outputGists(page) {
-    var temp = 'results' + page;
-    var description, a, text, tr, th, button, json, pageNum;
-    var tempOuter = document.getElementById(temp);
-    pageNum = 'Page' + page;
-    if (document.getElementById(pageNum))
-    {
-      var hide = document.getElementById(pageNum);
-      console.log('Page exists');
-      tempOuter.removeChild(hide);
-    }
-  //  var sep = document.creatElement('sep');
-  //  sep.setAttribute('id', pageNum);
-    var table = document.createElement('table');
-    var tbody = document.createElement('tbody');
-    //loop through to create pages
-    for (var i in sessionStorage)
-    {
-      var json = JSON.parse(sessionStorage.getItem(i));
-      //code to fix browser inconsistencies
-      if (json == null)
-      {
-        break;
-      }
-      if (json.page == page)
-      {
-        //creat button element
-        button = document.createElement('input');
-        button.setAttribute('type', 'button');
-        button.setAttribute('value', 'FAV');
-        button.setAttribute('name', i);
-        button.setAttribute('onclick', 'faveRow(this.name)');
-      }
-    }
+  else
+  {
+    var item = document.createElement('div');
+    item.innerHTML += '<a href ="' +
+        array[i].html_url + '"> ' + array[i].description + ' </a>';
+    conDiv.appendChild(item);
   }
+  this.fButton = document.createElement('button');
+  this.fButton.innerHTML = 'Favorite';
+  this.fButton.onclick = function()
+  {
+    localStorage.setItem('togist', JSON.stringify(this.parentNode.textContent));
+    this.parentNode.style.display = 'none';
+    createDisplaylist();
+  };
+  conDiv.appendChild(fButton);
+  ul.appendChild(conDiv);
+  list.appendChild(ul);
+  // var favoriteButton = document.createElement('input');
+  // favoriteButton.innerHTML = '+';
+  // favoriteButton.type = 'button';
+  // favoriteButton.value = 'favorite';
+  // favoriteButton.setAttribute('onclick', 'addtoFavorite(this.parentNode)');
+  // conDiv.appendChild(favoriteButton);
+  // favoriteButton.onclick = function()
+  // {
+  //   var conDiv = this.getAttribute('onclick');
+  // //  var tobeFavoredGist = findById('');
+  //   for (var i = 0; i < gistsArray.length; i++)
+  //   {
+  //     if (gistsArray[i].id === favoriteButton.id)
+  //     {
+  //     localStorage.setItem(favoriteButton.id, JSON.stringify(gistsArray[i]));
+  //       break;
+  //     }
+  //   }
+  // };
+  // list.appendChild(conDiv);
+  }
+  return list;
+}
+//create the display list for the favorites clicked on
+function createDisplaylist()
+{
+  var list = document.getElementById('fav-gist');
+  var ul = document.createElement('ul');
+  for (var key in localStorage)
+  {
+    var temp = document.createElement('li');
+    temp.innerHTML = localStorage[key];
+    this.dButton = document.createElement('button');
+    this.dButton.innerHTML = 'Remove Favorite';
+    this.dButton.onclick = function()
+    {
+      this.parentNode.style.display = 'none';
+      localStorage.clear();
+      createDisplaylist();
+    };
+    temp.appendChild(dButton);
+    ul.appendChild(temp);
+    list.appendChild(ul);
+  }
+  return list;
+}
